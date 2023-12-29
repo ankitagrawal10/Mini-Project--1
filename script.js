95ba67ff341815ad1dd25de8d6fceedb0a23ed6a
YourGitHubUsername <YourGitHubEmail>
2023-12-29 22:23:10 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,77 @@
+let boxes = document.querySelectorAll(".box");
+let resestbtn = document.querySelector("#reset-btn");
+let newgamebtn = document.querySelector("#new-btn");
+let msgcontainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turnO = true;
+const winpatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    console.log("box was clicked");
+    if (turnO) {
+      // player 1 turn
+      box.innerText = "O";
+      turnO = false;
+    } else {
+      box.innerText = "X"; // player 2 turn
+      turnO = true;
+    }
+    box.disabled = true;
+
+    checkwinner();
+  });
+});
+
+const restgame = () => {
+  trueO = true;
+  enableboxes();
+  msgcontainer.classList.add("hide");
+};
+
+const disabled = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+};
+
+const enableboxes = () => {
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+  }
+};
+
+const showWinner = (winner) => {
+  msg.innerText = `Congratulations, winner is ${winner}`;
+  msgcontainer.classList.remove("hide");
+  disabled();
+};
+
+const checkwinner = () => {
+  for (let pattern of winpatterns) {
+    let pos1val = boxes[pattern[0]].innerText;
+    let pos2val = boxes[pattern[1]].innerText;
+    let pos3val = boxes[pattern[2]].innerText;
+
+    if (pos1val != "" && pos2val != "" && pos3val != "") {
+      if (pos1val === pos2val && pos2val === pos3val) {
+        console.log("winner", pos1val);
+        showWinner(pos1val);
+      }
+    }
+  }
+};
+
+newgamebtn.addEventListener("click", restgame);
+resestbtn.addEventListener("click", restgame);
